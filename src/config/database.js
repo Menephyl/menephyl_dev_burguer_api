b8b0e989395438f4aa770311e098f17ace788522
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-10-22 14:57:37 -0300

    init config database

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+module.exports={
+    dialect: 'postgres',
+    host: 'localhost',
+    port : 5432,
+    username:'admin',
+    password:'admin',
+    database:'dev-burguer-db',
+    define:{
+        timestamps: true,
+        underscored:true,
+        underscoredAll:true,
+    }
+}
+
+// usuarios -> nome, email,telefone, data criação , data atualização, 
+// nome, email,telefone , data_criação,dataCriação
