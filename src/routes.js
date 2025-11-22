66fee2e222ca8012849eadf9b913b0300bc90a4d
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-11-22 06:09:02 -0300

    new migration for category data in table

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import SessionController from './app/controllers/SessionController.js';
 import multer from 'multer';
 import multerConfig from './config/multer.cjs';
 import authMiddleware from './middlewares/auth.js';
-
+import CategoryController from './app/controllers/CategoryController.js';
 const routes = new Router();
 
 const upload = multer(multerConfig);
@@ -18,4 +18,7 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 routes.post('/products', upload.single('file'),ProductController.store);
 routes.get('/products',ProductController.index);
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories',CategoryController.index);
 export default routes;
