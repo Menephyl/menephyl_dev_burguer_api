76ae75f1e0bb05c320d744e6b73b2039c08ff641
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-12-12 16:42:15 -0300

    Refactor routes for improved organization and consistency

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-
 import { Router } from 'express';
 
 import ProductController from './app/controllers/ProductController.js';
@@ -6,25 +5,36 @@ import UserController from './app/controllers/UserController.js';
 import SessionController from './app/controllers/SessionController.js';
 import multer from 'multer';
 import multerConfig from './config/multer.cjs';
-import authMiddleware from './middlewares/auth.js';
 import CategoryController from './app/controllers/CategoryController.js';
-import adminMiddleware from './middlewares/admin.js';
 import OrderController from './app/controllers/OrderController.js';
+import adminMiddleware from './app/middlewares/admin.js';
+import authMiddleware from './app/middlewares/auth.js';
+
 const routes = new Router();
 
 const upload = multer(multerConfig);
-// método http POST 
+// método http POST
 routes.post('/users', UserController.store);
-routes.post('/session', SessionController.store);
+routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);adminMiddleware
-routes.post('/products', upload.single('file'),ProductController.store);
+routes.use(authMiddleware);
+adminMiddleware;
+routes.post('/products', upload.single('file'), ProductController.store);
 
-routes.put('/products/:id', upload.single('file'),ProductController.update);
-routes.get('/products',ProductController.index);
-routes.post('/categories', upload.single('file'),CategoryController.store);
-routes.post('/categories',adminMiddleware ,CategoryController.store);
-routes.post('/orders',adminMiddleware ,OrderController.store);
-routes.put('/categories/:id',adminMiddleware, upload.single('file'),CategoryController.update);
-routes.get('/categories',CategoryController.index);
-export default routes; 
+routes.put('/products/:id', upload.single('file'), ProductController.update);
+routes.get('/products', ProductController.index);
+routes.post('/categories', upload.single('file'), CategoryController.store);
+routes.post('/categories', adminMiddleware, CategoryController.store);
+routes.put(
+  '/categories/:id',
+  adminMiddleware,
+  upload.single('file'),
+  CategoryController.update,
+);
+routes.get('/categories', CategoryController.index);
+
+routes.get('/orders', OrderController.index);
+
+routes.post('/orders', OrderController.store);
+routes.put('/orders/:id', adminMiddleware, OrderController.update);
+export default routes;
