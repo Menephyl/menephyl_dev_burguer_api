a7ec852d62d052cb10107360428688c4894ece55
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-12-18 19:44:06 -0300

    feat: Set up initial Express application with CORS, body parsing, and environment variable configuration.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 /** biome-ignore-all assist/source/organizeImports: <explanation> */
+import cors from 'cors';
 import express from 'express';
-
+// import { resolve } from 'node:path';
 import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
+
+
 const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use('/product-file',fileRouteConfig);
-app.use('/category-file',fileRouteConfig);
+
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(
+
+    // origin: process.env.CORS_ORIGIN
+
+
+));
+
+app.use('/product-file', fileRouteConfig);
+app.use('/category-file', fileRouteConfig);
+
 app.use(routes)
 export default app;
